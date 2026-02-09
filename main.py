fff3060d95f3a64a6d2cb72806cc78444853b110
Abhishek A M <154207679+123abhis@users.noreply.github.com>
2026-02-09 16:51:29 +0530

    Clean up main.py by removing commented code
    Removed commented-out code for the main pipeline execution.

--- main.py
+++ main.py
@@ -1,37 +1,3 @@
-# from src.data_ingestion import ingest_data
-# from src.data_validation import  validate_raw_data
-# from src.data_preprocessing import preprocess_data
-
-
-# from src.store_to_clickhouse import store_to_clickhouse
-
-# def main():
-#     print("Starting IVF Trigger Day ML Pipeline")
-
-#     print("Data Ingestion started...")
-#     ingest_data()
-
-#     print("Data Validation started...")
-#     validate_raw_data()
-    
-
-#     print("Data Preprocessing started...")
-#     preprocess_data()
-
-#     print(" Pipeline completed successfully!")
-
-
-#     print("Storing processed data to ClickHouse...")
-#     store_to_clickhouse()
-
-
-
-
-# if __name__ == "__main__":
-#     main()
-
-
-
 
 import time
 import os
